280a791ffe4db82bc1b7fb8627484508ae251247
nihatonal <onalnihat1986@gmail.com>
2023-06-23 09:29:13 +0300

    updated

--- frontend/src/main/Main.js
+++ frontend/src/main/Main.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Hero from './hero/Hero';
 import AboutUs from './aboutus/AboutUs';
+import Reviews from './reviews/Reviews';
+import Gallery from './gallery/Gallery';
 import './Main.css';
 function Main(props) {
     return (
         <div className="main_container">
             <Hero />
             <AboutUs />
+            <Reviews />
+            <Gallery />
         </div>
     );
 }
--- frontend/src/main/aboutus/AboutUs.js
+++ frontend/src/main/aboutus/AboutUs.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import host from '../../assets/images/host.jpg';
-import washing from '../../assets/images/washing_machine.jpeg';
-import bedroom from '../../assets/images/oda_1.jpeg'
+import washing from '../../assets/images/washing_machine - Copy.jpeg';
+import bedroom from '../../assets/images/room_aboutus.jpeg';
+import kitchen from '../../assets/images/room2_8.jpeg';
+import location from '../../assets/images/location.png';
+import airconditioner from '../../assets/images/airconditioner.jpeg';
+import wifi from '../../assets/images/wifi.jpg';
 import './AboutUs.css'
 function AboutUs(props) {
     return (
-        <section className='section_container' id='aboutus'>
-            <div className="section_wrapper">
+        <section className='section_container aboutus_container' id='aboutus'>
+            <div className="section_wrapper aboutus_wrapper" >
                 <h3 className="section_title">About us</h3>
                 <div className="aboutus_content">
                     <div className="aboutus_content_item">
@@ -19,15 +23,15 @@ function AboutUs(props) {
                         <p className="aboutus_content_item_desc">comfy bedrooms</p>
                     </div>
                     <div className="aboutus_content_item">
-                        <img src={host} alt="host" className="aboutus_content_image" />
+                        <img src={kitchen} alt="kitchen" className="aboutus_content_image" />
                         <p className="aboutus_content_item_desc">full equipped kitchens</p>
                     </div>
                     <div className="aboutus_content_item">
-                        <img src={host} alt="host" className="aboutus_content_image" />
-                        <p className="aboutus_content_item_desc">full equipped kitchens</p>
+                        <img src={airconditioner} alt="airconditioner" className="aboutus_content_image" />
+                        <p className="aboutus_content_item_desc">air-conditioner</p>
                     </div>
                     <div className="aboutus_content_item">
-                        <img src={host} alt="host" className="aboutus_content_image" />
+                        <img src={wifi} alt="wifi" className="aboutus_content_image" />
                         <p className="aboutus_content_item_desc">free wifi</p>
                     </div>
                     <div className="aboutus_content_item">
@@ -35,7 +39,7 @@ function AboutUs(props) {
                         <p className="aboutus_content_item_desc">washing and dryer machine</p>
                     </div>
                     <div className="aboutus_content_item">
-                        <img src={host} alt="host" className="aboutus_content_image" />
+                        <img src={location} alt="location" className="aboutus_content_image" />
                         <p className="aboutus_content_item_desc">great location</p>
                     </div>
                     <div className="aboutus_content_item">
@@ -43,10 +47,13 @@ function AboutUs(props) {
                         <p className="aboutus_content_item_desc">parking</p>
                     </div>
                 </div>
+
                 <p className="aboutus_desc">
                     We know what standarts of service are and we do our best to provide
                     you with the best experience in Fethiye! Our doors are always open for you!
                 </p>
+                <div className="olive_container"></div>
+                <div className="olive_container_2"></div>
             </div>
         </section>
     );
